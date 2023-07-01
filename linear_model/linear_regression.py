1e25e7b0a07df89e14a012ba3a7ef1ca60081e8e
prfnv <pentalbymf@mail.ru>
2023-07-01 15:13:14 +0300

    add sgd

--- linear_model/linear_regression.py
+++ linear_model/linear_regression.py
@@ -1,6 +1,8 @@
+import random
 import logging
 import numpy as np
 import pandas as pd
+from typing import Union, Callable
 
 
 _logger = logging.getLogger(__name__)
@@ -10,11 +12,13 @@ class MyLineReg:
     def __init__(
         self,
         n_iter: int = None,
-        learning_rate: float = None,
+        learning_rate: Union[float, Callable] = None,
         metric: str = None,
         reg: str = None,
         l1_coef: float = 0.0,
-        l2_coef: float = 0.0
+        l2_coef: float = 0.0,
+        sgd_sample: Union[int, float] = None,
+        random_state: int = 42
 ) -> None:
         self.n_iter = n_iter
         self.learning_rate = learning_rate
@@ -23,11 +27,13 @@ class MyLineReg:
         self.reg = reg
         self.l1_coef = l1_coef
         self.l2_coef = l2_coef
+        self.sgd_sample = sgd_sample
+        self.random_state = random_state
         
     def __repr__(self) -> str:
         return f"{self.__class__.__name__} class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
     
-    def _get_bias(self, X: pd.DataFrame = None):
+    def _get_bias(self, X: pd.DataFrame = None) -> pd.DataFrame:
         features = X.copy()
         list_ones = np.ones(X.shape[0], dtype="int8")
         features.insert(loc=0, column="ones", value=list_ones)
@@ -82,34 +88,48 @@ class MyLineReg:
         return reg
 
     def fit(self, X: pd.DataFrame = None, y: pd.Series = None, verbose: int = 0) -> None:
+        random.seed(self.random_state)
         features = self._get_bias(X)
-        self.weights = np.ones(features.shape[1], dtype="int8")
+        self.weights = np.ones(features.shape[1])
 
         for i in range(self.n_iter):
             y_hat = features @ self.weights
             loss = (y_hat - y)
             mse = ((loss.to_numpy() ** 2).sum() / loss.size)
-            grad = loss @ features * 2 / features.shape[0]
+            if self.sgd_sample is not None:
+                if isinstance(self.sgd_sample, float):
+                    self.sgd_sample = round(features.shape[0] * self.sgd_sample)
+                sample_rows_idx = random.sample(range(features.shape[0]), self.sgd_sample)
+                grad = loss.iloc[sample_rows_idx] @ features.iloc[sample_rows_idx, :] * 2 / self.sgd_sample
+            else:
+                grad = loss @ features * 2 / features.shape[0]
+                
             if self.reg is not None:
                 grad += self._regularization(
                     reg=self.reg,
                     l1_coef=self.l1_coef,
                     l2_coef=self.l2_coef,
                     weights=self.weights
-                ) 
-            self.weights = self.weights - self.learning_rate * grad.to_numpy()
-            if verbose and self.metric and not i % verbose:
-                self.score = getattr(self, "_" + self.metric)(y, y_hat)
-                _logger.info(f"{i if i else 'start'} | loss: {mse} | {self.metric}: {self.score}")
-            else:
-                _logger.info(f"{i if i else 'start'} | loss: {mse}")
+                )
+
+            if isinstance(self.learning_rate, Callable):
+                self.weights -= self.learning_rate(i+1) * grad.to_numpy()
+            else: 
+                self.weights -= self.learning_rate * grad.to_numpy()
+
+            if verbose and i % verbose == 0:
+                if self.metric:
+                    self.score = getattr(self, "_" + self.metric)(y, y_hat)
+                    _logger.info(f"{i if i else 'start'} | loss: {mse} | {self.metric}: {self.score}")
+                else:
+                    _logger.info(f"{i if i else 'start'} | loss: {mse}")
                 
         y_pred = self.predict(X)
         if self.metric:
             self.score = getattr(self, "_" + self.metric)(y, y_pred)
                 
     def get_coef(self):
-        return np.sum(self.weights[1:])
+        return np.mean(self.weights[1:])
     
     def predict(self, X: pd.DataFrame = None) -> float:
         features = self._get_bias(X)
@@ -119,8 +139,3 @@ class MyLineReg:
     
     def get_best_score(self) -> float:
         return self.score
-
-
-if __name__ == "__main__":
-    lin_reg = MyLineReg(n_iter=10, learning_rate=0.5)
-    print(lin_reg)
