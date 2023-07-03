17bcf89e407ef5b0a56906d7d89c534ae69c9765
prfnv <pentalbymf@mail.ru>
2023-07-04 00:10:21 +0300

    update entropy

--- tree/decision_tree_classifier.py
+++ tree/decision_tree_classifier.py
@@ -3,14 +3,25 @@ import numpy as np
 from typing import Tuple
 
 
-def _information_gain(y: pd.Series = None) -> float:
-    if y.nunique() != 1:
-        first_class = ((y.value_counts()[0] / y.shape[0]) * np.log2(y.value_counts()[0] / y.shape[0]))
-        second_class = ((y.value_counts()[1] / y.shape[0]) * np.log2(y.value_counts()[1] / y.shape[0]))
+def calculate_entropy(y: pd.Series = None) -> float:
+    _, counts = np.unique(y, return_counts=True)
 
-        return -(first_class + second_class)
-        
-    return 0
+    probabilities = counts / counts.sum()
+    entropy = sum(probabilities * -np.log2(probabilities))
+     
+    return entropy
+
+
+def calculate_overall_metric(left: pd.Series = None, right: pd.Series = None) -> float:
+    n = len(left) + len(right)
+    left_shape = len(left) / n
+    right_shape = len(right) / n
+
+    overall_metric = (
+        left_shape * calculate_entropy(left) + right_shape * calculate_entropy(right)
+    )
+    
+    return overall_metric
 
 
 def get_best_split(
@@ -19,31 +30,32 @@ def get_best_split(
 ) -> Tuple[str, float, float]:
     best_thresholds = {}
     for col in X.columns:
-        ig_start = _information_gain(y=y)
+        initial_entropy = calculate_entropy(y=y)
         unique_values = sorted(X[col].unique())
         thresholds = [np.mean([unique_values[i], unique_values[i+1]]) for i in range(len(unique_values)-1)]
         for threshold in thresholds:
             left = X[X[col] <= threshold][[col]].index.values
             right = X[X[col] > threshold][[col]].index.values
+
+            left_series = y.iloc[left]
+            right_series = y.iloc[right]
             
-            ig_left = _information_gain(y=y.iloc[left])
-            ig_right = _information_gain(y=y.iloc[right])
-            ig = ig_start - y.iloc[left].shape[0] / y.shape[0] * ig_left - y.iloc[right].shape[0] / y.shape[0] * ig_right
+            current_overall_metric = initial_entropy - calculate_overall_metric(left=left_series, right=right_series)
 
             if col not in best_thresholds:
                 best_thresholds[col] = {
-                    'split_value': threshold,
-                    'ig': ig
+                    "split_value": threshold,
+                    "metric": current_overall_metric
                 }
-            elif ig > best_thresholds[col]['ig']:
-                best_thresholds[col]['split_value'] = threshold
-                best_thresholds[col]['ig'] = ig
+            elif current_overall_metric > best_thresholds[col]["metric"]:
+                best_thresholds[col]["split_value"] = threshold
+                best_thresholds[col]["metric"] = current_overall_metric
             else:
                 continue
 
-    best = sorted(best_thresholds.items(), key=lambda x: x[1]['ig'], reverse=True)[0]
+    best = sorted(best_thresholds.items(), key=lambda x: x[1]["metric"], reverse=True)[0]
 
-    return best[0], best[1]['split_value'], best[1]['ig']
+    return best[0], best[1]["split_value"], best[1]["metric"]
 
 
 class MyTreeClf:
@@ -56,17 +68,12 @@ class MyTreeClf:
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
+        self.leafs_cnt = None
 
     def __repr__(self) -> str:
         params = [f"{key}={value}" for key, value in self.__dict__.items()]
 
         return f"{self.__class__.__name__} class: " + ", ".join(params)
-
-
-if __name__ == "__main__":
-    data = pd.read_csv('/Users/paparfen/vscode/ml_algorithms/data/data_banknote_authentication.txt', sep=",", header=None)
-    data.columns = ["col_1", "col_2", "col_3", "col_4", 'col_5']
-    X = data.drop('col_5', axis=1)
-    y = data['col_5']
-
-    print(get_best_split(X=X, y=y))
+    
+    def fit(self, X: pd.DataFrame = None, y: pd.Series = None, verbose: int = 0) -> None:
+        pass
